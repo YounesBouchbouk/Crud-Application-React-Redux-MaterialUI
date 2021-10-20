4c339a58e5f934f75142fc2e5dcb1f25bf408eef
younes bouchbouk <younesbouchbouk.py@gmail.com>
2021-10-20 16:11:24 +0100

    Reminder-Redux-MUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react' 
+import {
+       BrowserRouter as Router,
+       Switch,
+       Route,
+     } from "react-router-dom";
+import { connect } from 'react-redux'
+import {add_reminder , deletereminder ,clearrlinder ,editereminder} from './action/actionliste'
+import Home from "./pages/Home.jsx"
+import Header from './components/header.jsx'
+import background from "./assets/background.JPG"
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import { useTheme  , ThemeProvider} from "@material-ui/core/styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function  App ()  {
+       const theme = useTheme();
+       return (
+
+              <ThemeProvider theme={theme}>
+              <Router>
+                       
+                        <div style={{
+                                backgroundImage:`url(${background})`,
+                                backgroundSize:"cover",
+                                backgroundRepeat:"no-repeat"
+                                
+                                }}>
+                                       <Header /> 
+                     <CssBaseline />
+
+                     
+                     
+                     
+                 <Switch>
+                  <Route path="/" exact>
+                  <Home />
+                  </Route>
+                  
+                 
+                </Switch>
+                </div>
+              </Router>
+              </ThemeProvider>
+       )
+     
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import reminder from './reducer/actioncreatores'
+import {Provider} from 'react-redux'
+
+const store = createStore(reminder)
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +20,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
